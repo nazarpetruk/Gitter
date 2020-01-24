1e2d9a340f73c87c975c06d9fc91989f3bb6c6e5
nazar petruk <nazarpetruk15@gmail.com>
2020-01-24 19:24:54 +0100

    -tableview delegate methods added -table cell eded - outlets connected

--- Gitter/Controllers/TrandingVC.swift
+++ Gitter/Controllers/TrandingVC.swift
@@ -8,20 +8,35 @@
 
 import UIKit
 
-class TrandingVC: UIViewController {
+class TrandingVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //MARK: IBOutlets
-    
-    
-
-    
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
     }
-
-
+    
+    //MARK: TableViewProtocol methods
+    
+    func  numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "trendingRepoCell", for: indexPath) as? TrendingRepoCell else { return UITableViewCell() }
+        let repository = Repository(name: "RxSwift", image: UIImage(named: "searchIconLarge")!, descript: "Reactive programming library", forks: 2392, language: "Swift", contributors: 2386, repoUrl: "www.google.com")
+        cell.configCell(repo: repository)
+        return cell
+    }
 }
 
---
+++ Gitter/View/TrendingRepoCell.swift
@@ -0,0 +1,58 @@
+//
+//  TrendingRepoCell.swift
+//  Gitter
+//
+//  Created by Nazar Petruk on 24/01/2020.
+//  Copyright © 2020 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class TrendingRepoCell: UITableViewCell {
+
+    //MARK: IBOutlets
+    @IBOutlet weak var repoImgView: UIImageView!
+    @IBOutlet weak var repoNameLbl: UILabel!
+    @IBOutlet weak var repoDescLbl: UILabel!
+    @IBOutlet weak var numberOfForksLbl: UILabel!
+    @IBOutlet weak var languageLbl: UILabel!
+    @IBOutlet weak var contributorsNmbrLbl: UILabel!
+    @IBOutlet weak var toreadMeBtn: RoundedButton!
+    @IBOutlet weak var backGroundView: UIView!
+    
+    
+    //Variables
+    
+    private var repoUrl : String?
+    
+    func configCell(repo: Repository) {
+        repoImgView.image = repo.img
+        repoNameLbl.text = repo.name
+        repoDescLbl.text = repo.description
+        numberOfForksLbl.text = String(repo.forksNmbr)
+        languageLbl.text = repo.language
+        contributorsNmbrLbl.text = String(repo.contributorsNmbr)
+        repoUrl = repo.repoUrl
+    }
+    
+    override func layoutSubviews() {
+        backGroundView.layer.cornerRadius = 15
+        backGroundView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        backGroundView.layer.shadowOpacity = 0.25
+        backGroundView.layer.shadowRadius = 5.0
+        backGroundView.layer.shadowOffset = CGSize(width: 0, height: 0)
+    
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //trendingRepoCell
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
