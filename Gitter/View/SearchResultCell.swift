ef80366c467c0a7628043475e23eb3078ec50f3c
nazar petruk <nazarpetruk15@gmail.com>
2020-01-26 16:46:00 +0100

    -search cell ui made - config func for cell added

---
+++ Gitter/View/SearchResultCell.swift
@@ -0,0 +1,44 @@
+//
+//  SearchResultCell.swift
+//  Gitter
+//
+//  Created by Nazar Petruk on 26/01/2020.
+//  Copyright © 2020 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var repositoryNameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var forksNbrLbl: UILabel!
+    @IBOutlet weak var languageLbl: UILabel!
+    @IBOutlet weak var repoImg: UIImageView!
+    @IBOutlet weak var backgroundVIEW: UIView!
+    
+    
+    //MARK: Variables
+    private var repoUrl : String?
+    
+    
+    func configureCell(repository : Repository) {
+        
+        repositoryNameLbl.text = repository.name
+        descriptionLbl.text = repository.description
+        forksNbrLbl.text = String(repository.forksNmbr)
+        languageLbl.text = repository.language
+        repoUrl = repository.repoUrl
+    }
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundVIEW.layer.cornerRadius = 15
+    }
+}
+  
